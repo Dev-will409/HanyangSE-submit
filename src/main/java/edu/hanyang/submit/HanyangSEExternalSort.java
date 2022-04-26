82384f2ece18290603aca0eba4c0ecc16400215f
ujma1234 <ujma1234@naver.com>
2022-04-26 13:51:40 +0900

    .DS_Store 	externalsorteval.sh 	src/test/java/edu/hanyang/ExternalSortTest.java

--- src/main/java/edu/hanyang/submit/HanyangSEExternalSort.java
+++ src/main/java/edu/hanyang/submit/HanyangSEExternalSort.java
@@ -1,7 +1,13 @@
 package edu.hanyang.submit;
 
+import java.io.DataInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
 import io.github.hyerica_bdml.indexer.ExternalSort;
+import org.apache.commons.lang3.tuple.MutableTriple;
 
 
 public class HanyangSEExternalSort implements ExternalSort {
@@ -16,6 +22,46 @@ public class HanyangSEExternalSort implements ExternalSort {
      */
     @Override
     public void sort(String infile, String outfile, String tmpdir, int blocksize, int nblocks) throws IOException {
-        // TODO: your code here...
+        //initial phase
+        ArrayList <MutableTriple<Integer, Integer, Integer>> dataArr = new ArrayList<>()(nElement);
+        ...
+
+        //n-way merge
+        _externalMergesort(tmpdir, outfile, 0);
     }
-}
+
+    private void _externalMergesort(String tmpDir, String Outputfile, int step) throws IOException {
+        File[] fileArr = (new File (tmpDir + File.separator + String.valueOf(prevStep))).listFiles();
+        if (fileArr.length <= nblocks -1 ) {
+            for (FIle f : fileArr) {
+                DataInputStream Dos = new DataInputStream(f.getAbsolute.Path(), blocksize);
+                ...
+            }
+        }
+        else {
+            for (FIle f : file Arr) {
+
+
+                cnt++;
+                if(cnt == nblocks - 1) {
+                    n_way_merge(...);
+                }
+            }
+            _externalMergesort(tmpDir, outputfile, step+1);
+        }
+    }
+
+    public void n_way_merge(List<DataInputStream> files, String outputFile) throws IOException {
+        priorityQueue<DataManager> queue = new PriorityQueue<>(files.size(), new Comparator<DataManager>() {
+            public int compare(DataManager o1, dataManager o2) {
+                return o1.tuple.compareTo(o2.tuple);
+            }
+        });
+
+        while (queue.size() != 0) {
+            DataManager dm = queue.poll();
+            MutableTriple<Integer, Integer, Integer> tmp = dm.getTuple();
+            ...
+        }
+    }
+ }
