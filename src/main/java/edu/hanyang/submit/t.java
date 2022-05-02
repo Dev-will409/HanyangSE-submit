eb16d1de10b80f93f360835ba875cac3823faabc
ujma1234 <ujma1234@naver.com>
2022-05-02 21:23:40 +0900

    making init

--- src/main/java/edu/hanyang/submit/t.java
+++ src/main/java/edu/hanyang/submit/t.java
@@ -8,9 +8,32 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class t {
-    public void mergesort(ArrayList<MutableTriple<Integer, Integer, Integer>> dataArr, int nblocks) {
-        
+
+    public static ArrayList<MutableTriple<Integer, Integer, Integer>> mergesort(ArrayList<MutableTriple<Integer, Integer, Integer>> dataArr, int start, int end) {
+        if (start < end) {
+            ArrayList<MutableTriple<Integer, Integer, Integer>> tmp = new ArrayList<>(end);
+            int mid = (start + end) / 2;
+            mergesort(dataArr, start, mid);
+            mergesort(dataArr, mid + 1, end);
+            int p = start;
+            int q = mid + 1;
+            int idx = p;
+            while (p <= mid || q <= end) {
+                if (q > end || p <= mid && (dataArr.get(p).getLeft() <= dataArr.get(q).getLeft() && dataArr.get(p).getMiddle() <= dataArr.get(q).getMiddle() && dataArr.get(p).getRight() <= dataArr.get(q).getRight())) {
+                    tmp.add(dataArr.get(p++));
+                    idx++;
+                } else {
+                    tmp.add(dataArr.get(q++));
+                    idx++;
+                }
+            }
+            return tmp;
+        }
+        return null;
     }
+
+
+
     public static void main(String[] args) throws IOException {
         String path = "data/input_10000000.data";
         int nblocks = 5;
@@ -18,15 +41,16 @@ public class t {
         File in = new File (path);
         FileInputStream input = new FileInputStream(in);
         ArrayList<MutableTriple<Integer, Integer, Integer>> dataArr = new ArrayList<>(nblocks);
-        while (input.available()!=0){
+        while (input.available()!=0) {
             i++;
-            if(i==6) break;
+            if (i == 6) break;
             MutableTriple<Integer, Integer, Integer> tmp = new MutableTriple<>();
             tmp.setLeft(input.read());
             tmp.setMiddle(input.read());
             tmp.setRight(input.read());
             dataArr.add(tmp);
         }
-        System.out.print(dataArr);
+        ArrayList<MutableTriple<Integer, Integer, Integer>> temp = new ArrayList<>(nblocks);
+        System.out.print(mergesort(dataArr, 0, 4));
     }
 }
